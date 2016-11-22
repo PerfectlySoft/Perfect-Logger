b35e577350cda4e3b68b097a8fd3cc24cd5b362c
Jonathan Guthrie <jono@perfect.org>
2016-11-22 14:06:39 -0500

    Adding doc comments

--- Sources/Perfect_Logger.swift
+++ Sources/Perfect_Logger.swift
@@ -11,8 +11,6 @@
 import PerfectLib
 import SwiftMoment
 
-//public var logFileLocation = "./log.log"
-
 struct FileLogger {
 	let defaultFile = "./log.log"
 	let consoleEcho = ConsoleLogger()
@@ -64,43 +62,75 @@ struct FileLogger {
 	}
 }
 
-/// Placeholder functions for logging system
+/// Provision for logging information to a log file
 public struct LogFile {
 	private init(){}
 
 	static var logger = FileLogger()
+
+	/// The location of the log file.
+	/// The default location is relative, "log.log"
 	public static var location = "./log.log"
 
+	/// Logs a [DEBUG] message to the log file.
+	/// Also echoes the message to the console.
+	/// Specifiy a logFile parameter to direct the logging to a file other than the default.
+	/// Takes an optional "eventid" param, which helps to link related events together.
+	/// Returns an eventid string. If one was supplied, it will be echoed back, else a new one will be generated for reuse.
 	@discardableResult
 	public static func debug(_ message: @autoclosure () -> String, eventid: String = UUID().string, logFile: String = location) -> String {
 		LogFile.logger.debug(message: message(), eventid, logFile)
 		return eventid
 	}
 
+	/// Logs a [INFO] message to the log file.
+	/// Also echoes the message to the console.
+	/// Specifiy a logFile parameter to direct the logging to a file other than the default.
+	/// Takes an optional "eventid" param, which helps to link related events together.
+	/// Returns an eventid string. If one was supplied, it will be echoed back, else a new one will be generated for reuse.
 	@discardableResult
 	public static func info(_ message: String, eventid: String = UUID().string, logFile: String = location) -> String {
 		LogFile.logger.info(message: message, eventid, logFile)
 		return eventid
 	}
 
+	/// Logs a [WARNING] message to the log file.
+	/// Also echoes the message to the console.
+	/// Specifiy a logFile parameter to direct the logging to a file other than the default.
+	/// Takes an optional "eventid" param, which helps to link related events together.
+	/// Returns an eventid string. If one was supplied, it will be echoed back, else a new one will be generated for reuse.
 	@discardableResult
 	public static func warning(_ message: String, eventid: String = UUID().string, logFile: String = location) -> String {
 		LogFile.logger.warning(message: message, eventid, logFile)
 		return eventid
 	}
 
+	/// Logs a [ERROR] message to the log file.
+	/// Also echoes the message to the console.
+	/// Specifiy a logFile parameter to direct the logging to a file other than the default.
+	/// Takes an optional "eventid" param, which helps to link related events together.
+	/// Returns an eventid string. If one was supplied, it will be echoed back, else a new one will be generated for reuse.
 	@discardableResult
 	public static func error(_ message: String, eventid: String = UUID().string, logFile: String = location) -> String {
 		LogFile.logger.error(message: message, eventid, logFile)
 		return eventid
 	}
 
+	/// Logs a [CRIICAL] message to the log file.
+	/// Also echoes the message to the console.
+	/// Specifiy a logFile parameter to direct the logging to a file other than the default.
+	/// Takes an optional "eventid" param, which helps to link related events together.
+	/// Returns an eventid string. If one was supplied, it will be echoed back, else a new one will be generated for reuse.
 	@discardableResult
 	public static func critical(_ message: String, eventid: String = UUID().string, logFile: String = location) -> String {
 		LogFile.logger.critical(message: message, eventid, logFile)
 		return eventid
 	}
 
+	/// Logs a [EMERG] message to the log file.
+	/// Also echoes the message to the console.
+	/// Specifiy a logFile parameter to direct the logging to a file other than the default.
+	/// Takes an optional "eventid" param, which helps to link related events together.
 	public static func terminal(_ message: String, eventid: String = UUID().string, logFile: String = location) -> Never  {
 		LogFile.logger.terminal(message: message, eventid, logFile)
 		fatalError(message)
