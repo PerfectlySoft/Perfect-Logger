aeda334ee8cede33d8b517a5591c25bb0b30d3e8
Rockford Wei <rocky@perfect.org>
2018-01-30 12:50:53 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
 	name: "PerfectLogger",
 	targets: [],
 	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect.git", majorVersion: 3),
 		.Package(url: "https://github.com/iamjono/SwiftMoment.git", majorVersion: 0),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 3),
 	]
